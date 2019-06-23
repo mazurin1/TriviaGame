ab8a01da4dc727aae93ec6193cb4b7681dfeac76
Marian Azurin <maazurin@gmail.com>
2019-06-23 16:59:34 -0700

    initial game

---
+++ assets/javascript/app.js
@@ -0,0 +1,102 @@
+var myQuestions = [
+    {
+        question: "only interested in mainstream or popular things",
+        answers: {
+            a: 'basic',
+            b: 'woke',
+            c: 'slay',
+            d: 'ratchet',
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "really cool",
+        answers: {
+            a: 'shook',
+            b: 'dank',
+            c: 'savage',
+            d: 'trill',
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "to be honest",
+        answers: {
+            a: 'TBH',
+            b: 'FOMO',
+            c: 'TFW',
+            d: 'GMT',
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "when you are angry because you are so hungry",
+        answers: {
+            a: 'bougie',
+            b: 'suh',
+            c: 'cray',
+            d: 'hangry',
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "Someone from a higher class",
+        answers: {
+            a: 'bougie',
+            b: 'Gucci',
+            c: 'squad',
+            d: 'GOAT',
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "A group of friends",
+        answers: {
+            a: 'squad',
+            b: 'goals',
+            c: 'hunty',
+            d: 'bae',
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "upgrade appearance to a better version and enhance looks",
+        answers: {
+            a: 'lit',
+            b: 'fierce',
+            c: 'glow up',
+            d: 'thicc',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "to be bitter about something or someone",
+        answers: {
+            a: 'skurt',
+            b: 'throw shade',
+            c: 'salty',
+            d: 'roasted',
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "used to describe people having bigger body parts that are bigger than usual",
+        answers: {
+            a: 'thicc',
+            b: 'lit',
+            c: 'bop',
+            d: 'curve',
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "to give someone a nasty look or say something unpleasant about them",
+        answers: {
+            a: 'straight fire',
+            b: 'throw shade',
+            c: 'sip tea',
+            d: 'low key',
+        },
+        correctAnswer: 'b'
+    },
+];
