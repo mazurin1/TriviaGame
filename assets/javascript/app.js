cddebda8782c8ee7dd12dc25d541616e0ff1e3e7
Marian Azurin <maazurin@gmail.com>
2019-06-24 20:12:18 -0700

    js code final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,138 @@
 //start quiz
 //set Timer 2 minutes
+var timeout = 120;
+
 function start() {
 
     $('#quiz').show();
 
-    var timeout = 120;
-    var windowTimeout = setInterval(function () {
-        timeout--;
-        $("#showNumber").html("Time Remaining: " + timeout);
+    var correctAns = 0;
+    var incorrectAns = 0;
+    var unanswered = 0;
 
+    //timer, start and stop
+    var windowTimeout = setInterval(function () {
+        // if timer runs out, reset timer and hide quiz
+        if (timeout === 0) {
+            clearInterval(windowTimeout);
+            $('#quiz').hide();
+
+//if then statements for correct, incorrect and unanswered questions
+
+//q1
+            if ($('#q1:checked').val() == myAnswers[0]) {
+                correctAns++;
+            } else if ($('#q1:checked').length == 0) {
+                unanswered++;
+            }
+            else {
+                incorrectAns++;
+            } 
+
+//q2
+            if ($('#q2:checked').val() == myAnswers[1]) {
+                correctAns++;
+            } else if ($('#q2:checked').length == 0) {
+                unanswered++;
+            }
+            else {
+                incorrectAns++;
+            } 
+
+//q3
+            if ($('#q3:checked').val() == myAnswers[2]) {
+                correctAns++;
+            } else if ($('#q3:checked').length == 0) {
+                unanswered++;
+            }
+            else {
+                incorrectAns++;
+            } 
+
+//q4
+            if ($('#q4:checked').val() == myAnswers[3]) {
+                correctAns++;
+            } else if ($('#q4:checked').length == 0) {
+                unanswered++;
+            }
+            else {
+                incorrectAns++;
+            } 
+
+//q5
+            if ($('#q5:checked').val() == myAnswers[4]) {
+                correctAns++;
+            } else if ($('#q5:checked').length == 0) {
+                unanswered++;
+            }
+            else {
+                incorrectAns++;
+            } 
+
+//q6
+            if ($('#q6:checked').val() == myAnswers[5]) {
+                correctAns++;
+            } else if ($('#q6:checked').length == 0) {
+                unanswered++;
+            }
+            else {
+                incorrectAns++;
+            } 
+            
+//q7
+            if ($('#q7:checked').val() == myAnswers[6]) {
+                correctAns++;
+            } else if ($('#q7:checked').length == 0) {
+                unanswered++;
+            }
+            else {
+                incorrectAns++;
+            } 
+
+//q8
+            if ($('#q8:checked').val() == myAnswers[7]) {
+                correctAns++;
+            } else if ($('#q8:checked').length == 0) {
+                unanswered++;
+            }
+            else {
+                incorrectAns++;
+            } 
+
+//q9
+            if ($('#q9:checked').val() == myAnswers[8]) {
+                correctAns++;
+            } else if ($('#q9:checked').length == 0) {
+                unanswered++;
+            }
+            else {
+                incorrectAns++;
+            } 
+
+//q10
+            if ($('#q10:checked').val() == myAnswers[9]) {
+                correctAns++;
+            } else if ($('#q10:checked').length == 0) {
+                unanswered++;
+            }
+            else {
+                incorrectAns++;
+            } 
+
+            //show score in html
+            $('#correct-answers').html(correctAns);
+            $('#incorrect-answers').html(incorrectAns);
+            $('#unanswered').html(unanswered);
+            $('#results').show();
+            
+        } else {
+
+            //show timer in page
+            timeout--;
+            $("#showNumber").html("Time Remaining: " + timeout);
+        }
     }, 1000);
 }
-
-
-window.onload = function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#countdownTimer');
-    startTimer(fiveMinutes, display);
-};
-
 
 //10 questions, 4 possible answers, 1 correct answer
 var myQuestions = [
@@ -124,17 +238,10 @@ var myQuestions = [
     },
 ];
 
-function questions() {
+// array of correct answers
+var myAnswers = ['a', 'b', 'a', 'd', 'a', 'a', 'c', 'c', 'a', 'b'];
 
-}
-
-//score
-var score = function () {
-    if (input === correctAnswer) {
-        correct = correctAns + 1;
-        alert("Correct!");
-    } else {
-        incorrect = incorrectAns + 1;
-        alert("incorrect")
-    }
+//end game when timer hits 0
+function endGame () {
+    timeout = 0;
 }
